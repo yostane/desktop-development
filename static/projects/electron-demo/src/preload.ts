d04c8b00eef32d73200a0dd9742dedb196e94855
yostane <1958676+yostane@users.noreply.github.com>
2024-03-21 16:16:08 +0100

    some more code

--- static/projects/electron-demo/src/preload.ts
+++ static/projects/electron-demo/src/preload.ts
@@ -1,7 +1,7 @@
 // See the Electron documentation for details on how to use preload scripts:
 // https://www.electronjs.org/docs/latest/tutorial/process-model#preload-scripts
 import { contextBridge } from "electron";
-import fs from "node:fs";
+import fs from "node:fs/promises";
 
 contextBridge.exposeInMainWorld("versions", {
   node: () => process.versions.node,
@@ -9,9 +9,9 @@ contextBridge.exposeInMainWorld("versions", {
   electron: () => process.versions.electron,
 });
 
-window.addEventListener("DOMContentLoaded", () => {
-  fs.readdir("/", {}, (err, files) => {
-    document.getElementById("files").innerHTML = files.join(" - ");
-    console.log(files);
-  });
+window.addEventListener("DOMContentLoaded", async () => {
+  const files = await fs.readdir(".");
+  const html = files.map((file) => `<li>${file}</li>`).join("\n");
+  document.getElementById("files").innerHTML = `<ul>${html}</ul>`;
+  console.log(files);
 });
