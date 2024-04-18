e882054d2c4273c7bbefc73bdf96faf8b872abd1
yostane <1958676+yostane@users.noreply.github.com>
2024-04-18 15:38:41 +0200

    add login and logout

--- projects/electron-demo-03-firebase/src/components/AuthenticationComponent.tsx
+++ projects/electron-demo-03-firebase/src/components/AuthenticationComponent.tsx
@@ -1,19 +1,35 @@
 // Deux champs pour saisir l'email et le mdp et deux boutons pour se connecter et s'inscrire
 
+import { getAuth, signInWithEmailAndPassword, signOut } from "firebase/auth";
 import { useState } from "react";
+import { app } from "../firebase";
 
 export default function AuthenticationComponent() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("user@user.com");
+  const [password, setPassword] = useState("user@user.com");
+  const [loggedInEmail, setLoggedInEmail] = useState("");
 
-  function login() {}
+  async function login() {
+    console.log("login with", email, password);
+    const auth = getAuth(app);
+    const userInfo = await signInWithEmailAndPassword(auth, email, password);
+    setLoggedInEmail(userInfo.user.email);
+  }
 
-  function register() {}
+  async function logout() {
+    const auth = getAuth(app);
+    await signOut(auth);
+    setLoggedInEmail("");
+  }
+
+  function register() {
+    console.log("register with", email, password);
+  }
 
   return (
     <div className="form-group">
       <div>
-        <label htmlFor="email">Email: </label>
+        <label htmlFor="email">Email:</label>
         <input
           name="email"
           type="text"
@@ -22,7 +38,7 @@ export default function AuthenticationComponent() {
         />
       </div>
       <div>
-        <label htmlFor="email">Mot de passe: </label>
+        <label htmlFor="email">Mot de passe:</label>
         <input
           name="password"
           type="password"
@@ -31,8 +47,17 @@ export default function AuthenticationComponent() {
         />
       </div>
       <div>
-        <button onClick={login}>Login</button>
-        <button onClick={register}>Register</button>
+        {loggedInEmail.length > 0 ? (
+          <>
+            <p>Hello {loggedInEmail}</p>
+            <button onClick={logout}>Se déconnecter</button>
+          </>
+        ) : (
+          <>
+            <button onClick={login}>Se connecter</button>
+            <button onClick={register}>S'enregistrer</button>
+          </>
+        )}
       </div>
     </div>
   );
